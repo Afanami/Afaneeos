2c7e406bc2488bdb7c79e148eea628cec178e449
Afanami <afan6333@gmail.com>
2018-05-18 20:43:25 +1200

    adding feature to show user playlists

---
+++ src/components/UserPlaylistsList/UserPlaylistsList.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import UserPlaylists from "../UserPlaylists/UserPlaylists";
+import "./UserPlaylistsList.css";
+
+class TrackList extends Component {
+  render() {
+    return (
+      <div className="TrackList">
+        {this.props.tracks &&
+          this.props.tracks.map(track => {
+            return (
+              <Track
+                key={track.id}
+                track={track}
+                onAdd={this.props.onAdd}
+                onRemove={this.props.onRemove}
+                isRemoval={this.props.isRemoval}
+              />
+            );
+          })}
+      </div>
+    );
+  }
+}
+
+export default TrackList;
